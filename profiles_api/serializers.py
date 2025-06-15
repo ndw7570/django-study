7658fe94a9fc48977afbad1f6a7d431b98a7110a
ndwMango <ndw7570@mangoworldcar.com>
2025-06-15 20:10:43 +0900

    .
    .

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,38 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password') # password is not included in the fields because we want to use write_only = True
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
+
+    # UPDATE: Bug in profile serializer
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password') # validated_data에 password가 있으면 꺼내서(pop)
+            instance.set_password(password) # password는 해시 처리
+        return super().update(instance, validated_data) # 나머지 필드는 super().update()에 맡겨 기본 업데이트 로직을 실행하도록
+
