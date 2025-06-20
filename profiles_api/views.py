53f495ebc9e06f15649f953e703f1568052852b2
ndw7570 <145440577+ndw7570@users.noreply.github.com>
2025-06-20 14:45:15 +0900

    Django-분석
    데이터 저장 확인(SQL-Lite)

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from profiles_api import serializers
+from profiles_api import models
 
 # Django REST framework (DRF)
 class HelloApiView(APIView): # APIView는 Django의 RESTful API를 만들기 쉽게 도와주는, JSON 형식의 응답을 주는 API 뷰 (HTML을 보여주는 뷰가 아니라)
@@ -52,6 +53,7 @@ class HelloApiView(APIView): # APIView는 Django의 RESTful API를 만들기 쉽
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
         """Return a hello message"""
@@ -63,3 +65,39 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def unpdate(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
